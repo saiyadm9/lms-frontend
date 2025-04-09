6cbc3a67aac310779a4b74d00e0b0e7d3556bd43
Raju Islam <bdrajuislam246@gmail.com>
2025-04-09 11:48:58 +0600

    update on regiter route for amdin

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 
 const loginProtectedRoutes = ["/login"];
 const logoutProtectedRoutes = ["/profile"];
+const adminRoutes = ["/admin"];
 
 export async function middleware(req) {
   const cookieStore = cookies();
@@ -11,21 +12,19 @@ export async function middleware(req) {
 
   // If logged in and visiting login page, redirect to home
   if (token && loginProtectedRoutes.includes(path)) {
-    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+    return NextResponse.redirect(new URL("/", req.nextUrl.origin).toString());
   }
 
   // If not logged in and visiting profile page, redirect to home
   if (!token && logoutProtectedRoutes.includes(path)) {
-    return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+    return NextResponse.redirect(new URL("/", req.nextUrl.origin).toString());
   }
 
-  // Check if route is an admin route
-  const isAdminRoute = path.startsWith("/admin");
-
-  if (isAdminRoute) {
+  // Only allow admins to access /admin and /register
+  if (adminRoutes.includes(path)) {
     if (!token) {
       console.error("No token found for admin route");
-      return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+      return NextResponse.redirect(new URL("/", req.nextUrl.origin).toString());
     }
 
     try {
@@ -41,11 +40,13 @@ export async function middleware(req) {
 
       if (!response.ok) {
         console.error("JWT Verification Failed");
-        return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+        return NextResponse.redirect(
+          new URL("/", req.nextUrl.origin).toString()
+        );
       }
     } catch (error) {
       console.error("Admin verification error:", error);
-      return NextResponse.redirect(new URL("/", req.nextUrl.origin));
+      return NextResponse.redirect(new URL("/", req.nextUrl.origin).toString());
     }
   }
 
