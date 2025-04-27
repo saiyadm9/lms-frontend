44dd6daf9b1c517739b2859c277019c3c958273a
Contour2017 <studios.contour@gmail.com>
2025-04-27 21:33:26 +0600

    Undo Update

--- src/app/(frontend)/credit-course-ossd/learning-methods/page.js
+++ src/app/(frontend)/credit-course-ossd/learning-methods/page.js
@@ -33,25 +33,6 @@ const LearningMethods = () => {
               students not only achieve academic success but also develop
               valuable skills that will serve them well in their future careers.
             </p>
-            <p className="mt-4 text-gray-700 text-justify">
-              At Academy of Scholars, both virtual and online learning are
-              central to the school&apos;s innovative approach to education,
-              offering students flexibility and interactivity in their studies.
-              Virtual learning provides an immersive experience with live,
-              instructor-led classes, allowing students to actively participate
-              in real-time discussions, ask questions, and receive immediate
-              feedback, creating a dynamic and collaborative learning
-              environment. Online learning, on the other hand, offers 24/7
-              access to course materials, enabling students to study at their
-              own pace and fit their learning around other commitments. The
-              combination of these methods gives students the best of both
-              worlds: the flexibility to learn anytime, anywhere, alongside the
-              interactive, real-time connection with instructors and peers.
-              These advantages foster independent learning, enhance time
-              management skills, and provide continuous support, ensuring that
-              students not only achieve academic success but also develop
-              valuable skills that will serve them well in their future careers.
-            </p>
           </div>
 
           <div className="md:col-span-1 flex justify-center mt-6 md:mt-0">
